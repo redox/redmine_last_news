0ab72488e731dc4bac33b9e51382ac1a493bb315
Sylvain Utard - redox <sylvain.utard@gmail.com>
2011-10-02 19:44:15 +0200

    typo

--- lib/last_news_hooks.rb
+++ lib/last_news_hooks.rb
@@ -1,7 +1,7 @@
 class LastNewsHooks < Redmine::Hook::ViewListener
   
     def view_welcome_index_left(context = {})
-      context[:controller].render_to_string("/last_reviews/home_box", :locals => { :news => News.latest(User.current) }, :layout => false)
+      context[:controller].render_to_string("/last_news/home_box", :locals => { :news => News.latest(User.current) }, :layout => false)
     end
 
 end
